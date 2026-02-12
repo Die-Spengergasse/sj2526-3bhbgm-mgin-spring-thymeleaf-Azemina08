143d671e0de431c9591a496dad1721c38db18611
Azemina <Sil220044@spengergasse.at>
2026-02-12 15:35:13 +0100

    comitt

--- src/main/java/at/spengergasse/spring_thymeleaf/SpringThymeleafApplication.java
+++ src/main/java/at/spengergasse/spring_thymeleaf/SpringThymeleafApplication.java
@@ -2,7 +2,7 @@ package at.spengergasse.spring_thymeleaf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// springboot notation.
 @SpringBootApplication
 public class SpringThymeleafApplication {
 
